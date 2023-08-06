150ae11194feedbbd1268b101fd2ea07a3c18e6e
bernardo <baavieira@sga.pucminas.br>
2023-08-06 17:38:21 -0300

    indice da string

--- AEDS2 - Java/Main.java
+++ AEDS2 - Java/Main.java
@@ -1,29 +1,14 @@
-import java.util.Scanner;
 
+import java.util.Scanner;
 public class Main{
 
-    public static void main(String[] args){
+    public static void main(String [] args){
 
+        String palavra;
         Scanner scanner = new Scanner(System.in);
-
-        String palindromo = scanner.nextLine();
-
-        while(!palindromo.equals("FIM")) {
-
-            boolean a = true;
-
-            for (int i = 0; i < palindromo.length()/2 OR a; i++) {
-                if (palindromo.charAt(i) != palindromo.charAt(palindromo.length() - i - 1)) {
-                    a = false;
-                    System.out.println("NAO");
-                }
-            }
-            if (a)
-                System.out.println("SIM");
-
-
-            palindromo = scanner.nextLine();
-        }
-        scanner.close();
+        palavra = scanner.nextLine();
+        int indice;
+        indice = scanner.nextInt();
+        System.out.println("The character at position "+indice+" is "+ palavra.charAt(indice - 1));
     }
 }
