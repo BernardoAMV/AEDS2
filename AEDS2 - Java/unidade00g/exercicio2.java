1741ba94448fc04583aa62ece2cbcfba12e3667e
bernardo <baavieira@sga.pucminas.br>
2023-08-10 19:28:33 -0300

    att

--- AEDS2 - Java/unidade00g/exercicio2.java
+++ AEDS2 - Java/unidade00g/exercicio2.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class exercicio2 {
 
-    public static void main{
-
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        System.out.println("Insira a quantdade de numeros: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        for(int i = 0; i < n; i++){
+            int aleatorio = rand.nextInt(2000);
+            array[i] = aleatorio;
+            System.out.println("Número " + i + " " + array[i]);
+        }
 
     }
 }
