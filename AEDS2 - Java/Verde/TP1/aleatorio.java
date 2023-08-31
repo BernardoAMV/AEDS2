14e5a13a2b06f06eb3d4bbd741fba324461e845e
bernardo <baavieira@sga.pucminas.br>
2023-08-31 09:57:28 -0300

    TP1-aleatorio

--- AEDS2 - Java/Verde/TP1/aleatorio.java
+++ AEDS2 - Java/Verde/TP1/aleatorio.java
@@ -3,32 +3,33 @@ import java.util.Random;
 public class aleatorio {
 
     public static void main(String[] args){
+        Random gerador = new Random();
         String parada = "FIM";
         String frase= MyIO.readLine("");
         while(parada(frase, parada)){
-            aleatoriza(frase);
+            aleatoriza(frase, gerador);
             frase = MyIO.readLine("");
 
         }
 
     }
-    static void aleatoriza(String p){
-        Random gerador = new Random();
+    static void aleatoriza(String p, Random gerador){ // funcao para aleatorizar a string entrada
+
         gerador.setSeed(4);
+        char letraAleatoria1 = ((char)('a' + (Math.abs(gerador.nextInt() % 26))));
+        char letraAleatoria2 = ((char)('a' + (Math.abs(gerador.nextInt() % 26))));
         char aleatoria;
-        for(int i = 0; i < p.length(); i++){
-            char letraAleatoria1 = ((char)('a' + (Math.abs(gerador.nextInt()) % 26)));
-            char letraAleatoria2 = ((char)('a' + (Math.abs(gerador.nextInt()) % 26)));
+        for(int i = 0; i < p.length(); i++){ // a funcao gera a letra aleatoria, procura na string a letra em questao e printa a letra modificada
             if(p.charAt(i) == letraAleatoria1) {
                 aleatoria = letraAleatoria2;
                 MyIO.print(aleatoria);
-            }else {
+            }else // se nao houver semelhanca, a funcao printa a letra da propria string
                 MyIO.print(p.charAt(i));
-            }
+
         }
         MyIO.print("\n");
     }
-    static boolean parada(String palavra, String parada){
+    static boolean parada(String palavra, String parada){ //metodo para identificar a parada, FIM
         for(int i = 0; i < parada.length(); i++){
             if(palavra.charAt(i) != parada.charAt(i))
                 return true;
