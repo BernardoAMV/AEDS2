664082411f6445af0c1936ad993945e31d7d9511
bernardo <baavieira@sga.pucminas.br>
2023-10-14 11:10:02 -0300

    correcoes

---
+++ AEDS2 - Java/Verde/TP2/Java/insercao.java
@@ -0,0 +1,4 @@
+public class insercao {
+
+    public static main
+}
---
+++ AEDS2 - Java/Verde/TP2/Java/teste.java
@@ -0,0 +1,8 @@
+public class teste {
+
+    public static void main(String[] args){
+        String teste = "Joe fulks";
+
+        MyIO.println(teste.compareTo("JoeNorlander"));
+    }
+}
