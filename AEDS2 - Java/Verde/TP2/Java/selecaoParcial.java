0aba96ab854845e202efaf945b65d91577a8a89e
bernardo <baavieira@sga.pucminas.br>
2023-10-14 11:29:06 -0300

    Ex8 TP2 - selecaoParcial

---
+++ AEDS2 - Java/Verde/TP2/Java/selecaoParcial.java
@@ -0,0 +1,186 @@
+import java.util.*;
+import java.lang.*;
+
+class Jogador {
+    private int id;
+    private String nome;
+    private int altura;
+    private int peso;
+    private String universidade;
+    private int anoNascimento;
+    private String cidadeNascimento;
+    private String estadoNascimento;
+    public Jogador(){}
+    public Jogador(int id, String nome, int altura, int peso, String universidade, int anoNascimento, String cidadeNascimento, String estadoNascimento){
+        setId(id);
+        setNome(nome);
+        setAltura(altura);
+        setPeso(peso);
+        setUniversidade(universidade);
+        setAnoNascimento(anoNascimento);
+        setCidadeNascimento(cidadeNascimento);
+        setEstadoNascimento(estadoNascimento);
+    }
+    public int getId(){
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public String getCidadeNascimento() {
+        return cidadeNascimento;
+    }
+
+    public String getEstadoNascimento() {
+        return estadoNascimento;
+    }
+
+    public String getUniversidade() {
+        return universidade;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public void setCidadeNascimento(String cidadeNascimento) {
+        cidadeNascimento = validar(cidadeNascimento);
+        this.cidadeNascimento = cidadeNascimento;
+    }
+
+    public void setEstadoNascimento(String estadoNascimento) {
+        estadoNascimento = validar(estadoNascimento);
+        this.estadoNascimento = estadoNascimento;
+    }
+
+    public void setNome(String nome) {
+        nome = validar(nome);
+        this.nome = nome;
+    }
+    public void setUniversidade(String universidade) {
+        universidade = validar(universidade);
+        this.universidade = universidade;
+    }
+    public String validar(String str){
+        if(str.isEmpty())
+            str = "nao informado";
+        return str;
+    }
+    public Jogador clone(){
+        return new Jogador(getId(),getNome(),getAltura(),getPeso(),getUniversidade(),getAnoNascimento(),getCidadeNascimento(),getEstadoNascimento());
+    }
+
+
+    public void imprimir() {
+        System.out.println("[" + id + " ## " + nome + " ## " + altura + " ## " + peso + " ## " +anoNascimento + " ## " + universidade + " ## " + cidadeNascimento + " ## " + estadoNascimento + ']');
+    }
+    public void ler(String jogador){
+
+        String[] dados = jogador.split(",", 8);
+        setId(dados[0]!="" ?Integer.parseInt(dados[0]): -1);
+        setNome(dados[1]);
+        setAltura(Integer.parseInt(dados[2]));
+        setPeso(Integer.parseInt(dados[3]));
+        setUniversidade(dados[4]);
+        setAnoNascimento(Integer.parseInt(dados[5]));
+        setCidadeNascimento(dados[6]);
+        setEstadoNascimento(dados[7]);
+    }
+
+}
+public class selecaoParcial {
+    public static int cont;
+    public static int contMov;
+    public static long tempo;
+
+    public static void criarLog(){
+        Arq.openWrite("806347_selecao.txt");
+        Arq.println("806347\t" + tempo + "ms" + "\t" + cont + "comparacoes" + "\t" + contMov + "movimentacoes");
+        Arq.close();
+    }
+    public static void leArquivo(Jogador[] jogadores){
+        Arq.openRead("players.csv");
+        Arq.readLine(); //remove a primeira linha
+        for(int i = 0; i < 3922; i++){
+            String data = Arq.readLine();
+            jogadores[i] = new Jogador();
+            jogadores[i].ler(data);
+        }
+    }
+    //metodo para identificar a parada, FIM
+    static boolean parada(String palavra, String parada){
+        for(int i = 0; i < parada.length(); i++){
+            if(palavra.charAt(i) != parada.charAt(i))
+                return true;
+        }
+        return false;
+    }
+    public static void sort(Jogador [] array, int n, int k) {
+        for (int i = 0; i < k; i++) {
+            int menor = i;
+            for (int j = (i + 1); j < n; j++){
+                if (array[menor].getNome().compareTo(array[j].getNome()) > 0){
+                    cont++;
+                    menor = j;
+                }
+            }
+            Jogador tmp = new Jogador();
+            tmp = array[menor].clone();
+            array[menor] = array[i].clone();
+            array[i] = tmp.clone();
+            contMov += 3;
+        }
+    }
+
+    public static void main(String[] args){
+        int n = 3922;
+        int realTam = 0;
+        cont = 0;
+        Jogador[] jogadores = new Jogador[n];
+        Jogador[] vetor = new Jogador[n];
+
+        leArquivo(jogadores);
+        int posicao;
+        String parada = MyIO.readString();
+
+        while(parada(parada, "FIM")){
+            posicao = Integer.parseInt(parada);
+            vetor[realTam] = new Jogador();
+            vetor[realTam] = jogadores[posicao].clone();
+            realTam++;
+            parada = MyIO.readString();
+        }
+        long inicio = System.currentTimeMillis();
+        sort(vetor, realTam, 10);
+        long fim = System.currentTimeMillis();
+        for(int i = 0; i < 10; i++){
+            vetor[i].imprimir();
+        }
+        tempo = fim - inicio;
+        criarLog();
+    }
+}
